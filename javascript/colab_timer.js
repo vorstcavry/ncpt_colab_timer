760d09d89bff7dcef28c1608fdd31c5a3f95722a
Bagus Vendi Vannessa <vorstcavry@gmail.com>
2023-10-28 15:48:45 +0700

    Update colab_timer.js

--- javascript/colab_timer.js
+++ javascript/colab_timer.js
@@ -112,7 +112,7 @@ onUiLoaded(function () {
 
   let img = document.createElement("img");
   img.src =
-    "https://ssl.gstatic.com/colaboratory-static/common/de56aa663d279b80074b6c21f69dc872/img/favicon.ico";
+    "https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg";
   img.width = 24;
 
   let timerEl = document.createElement("div");
